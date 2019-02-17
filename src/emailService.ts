ce05698e8a2584b9fbded0a0e1e76f4805d36ef5
azaslonov <zaslonov.alexander@gmail.com>
2019-02-16 17:39:29 -0800

    unified object storage search queries

--- src/emailService.ts
+++ src/emailService.ts
@@ -1,4 +1,3 @@
-import { Bag } from "@paperbits/common/bag";
 /**
  * @license
  * Copyright Paperbits. All Rights Reserved.
@@ -7,7 +6,7 @@ import { Bag } from "@paperbits/common/bag";
  */
 
 import * as Utils from "@paperbits/common/utils";
-import { IObjectStorage } from "@paperbits/common/persistence";
+import { IObjectStorage, Query, Operator } from "@paperbits/common/persistence";
 import { IBlockService } from "@paperbits/common/blocks";
 import { Contract } from "@paperbits/common";
 import { EmailContract } from "./emailContract";
@@ -22,17 +21,18 @@ export class EmailService {
         private readonly blockService: IBlockService
     ) { }
 
-    private async searchByProperties(properties: string[], value: string): Promise<EmailContract[]> {
-        const result = await this.objectStorage.searchObjects<Bag<EmailContract>>(emailTemplatesPath, properties, value);
-        return Object.keys(result).map(key => result[key]);
-    }
-
     public async getEmailTemplateByKey(key: string): Promise<EmailContract> {
         return this.objectStorage.getObject<EmailContract>(key);
     }
 
-    public search(pattern: string): Promise<EmailContract[]> {
-        return this.searchByProperties(["title"], pattern);
+    public async search(pattern: string): Promise<EmailContract[]> {
+        const query = Query
+            .from<EmailContract>()
+            .where("title", Operator.contains, pattern)
+            .orderBy("title");
+
+        const result = await this.objectStorage.searchObjects<EmailContract>(emailTemplatesPath, query);
+        return Object.values(result);
     }
 
     public async deleteEmailTemplate(emailTemplate: EmailContract): Promise<void> {
