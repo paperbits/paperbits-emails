64fe2abe2a70371ee57d64c755db288a39037827
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-11-23 21:08:48 -0800

    tmp

--- src/section/sectionModel.ts
+++ src/section/sectionModel.ts
@@ -7,7 +7,7 @@
 
 import { WidgetModel } from "@paperbits/common/widgets";
 import { BackgroundModel } from "@paperbits/common/widgets/background";
-import { StyleContract } from "@paperbits/common/styles/styleConfig";
+import { StyleContract } from "@paperbits/common/styles";
 
 export class SectionModel {
     public widgets: WidgetModel[];
