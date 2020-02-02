ea3cfc25f6a4c34638fb2244f94c6b88e88fb2f5
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-02-01 22:18:37 -0800

    tmp

--- src/section/ko/sectionViewModelBinder.ts
+++ src/section/ko/sectionViewModelBinder.ts
@@ -47,7 +47,7 @@ export class SectionViewModelBinder implements ViewModelBinder<SectionModel, Sec
         viewModel.snapTo(model.snap);
 
         if (model.styles) {
-            viewModel.styles(await this.styleCompiler.getStyleModelAsync(model.styles, bindingContext));
+            viewModel.styles(await this.styleCompiler.getStyleModelAsync(model.styles, bindingContext.styleManager));
         }
 
         const binding: IWidgetBinding<SectionModel> = {
